bc5855bd06c40cce0961a84bd8348269e893cd85
Akshay <akshayy976@gmail.com>
2021-11-01 14:50:36 +0530

    token correction

--- app.py
+++ app.py
@@ -20,14 +20,15 @@ app.config['SECRET_KEY'] = "499baae0-223a-4e68-b72e-eb0719d0ee37"
 def token_required(func):
     @wraps(func)
     def decorated(*args, **kwrags):
+        token=""
         if 'Authorization' in request.headers:
             data = request.headers['Authorization']
             token = str.replace(str(data), 'Bearer ', '')
         # token = request.args.get('token')
         # token="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6NywiZXhwIjoxNjM1NzUwMjk0fQ.UbVtIq3ftwFgj4qig22kE9CFH0KhqG2f2A7ygnUxxhE"
-        print("---------token-",token)
+            print("---------token-",token)
         if not token:
-            return jsonify({'Token is missing'})
+            return jsonify({"message":'Token is missing'},500)
         try:
             payload = jwt.decode(token, app.config['SECRET_KEY'],algorithms=["HS256"])
             print("payload-----",payload)
